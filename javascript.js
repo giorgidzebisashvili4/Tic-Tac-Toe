47e3291cccdc783357ed2488d42e5174f3e062f4
giorgidzebisashvili4 <giorgidzebisashvili4@gmail.com>
2023-12-12 23:02:50 +0400

    stop Game function fail

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const boardDisplay = document.querySelector('.container')
 
-
+// create DOM grid 3x3
 for(let i=0 ; i<9; i++){
 let grid = document.createElement('div')
 grid.classList= `grid-${i}`
@@ -11,34 +11,60 @@ boardDisplay.appendChild(grid)
 let allGrid = document.querySelectorAll('.container>div')
 
 
-
-
 function createUser () {
     let arr = []
-        
-          
-    const marcko = (x) => arr[x]= ("o");
-    const marckx = (x) => arr[x]= ("x");
-  
-    
 
-    return { arr, marcko,marckx};
+    let testScore = function(){
+        if(
+            (arr[0] === arr[1] & arr[1] === arr[2]) && arr[0]   ||
+            (arr[3] === arr[4] & arr[4] === arr[5]) && arr[3]   ||
+            (arr[6] === arr[7] & arr[7] === arr[8]) && arr[6]   ||
+            (arr[0] === arr[3] & arr[3] === arr[6]) && arr[0]   ||
+            (arr[1] === arr[4] & arr[4] === arr[7]) && arr[1]   ||
+            (arr[2] === arr[5] & arr[5] === arr[8]) && arr[2]   ||
+            (arr[0] === arr[4] & arr[4] === arr[8]) && arr[0]   ||
+            (arr[2] === arr[4] & arr[4] === arr[6]) && arr[2]  
+
+        ){
+            console.log(`winner is ${winner}`)
+            stopGame()
+        }else if(movesMade === 9){
+            console.log('it is tie')
+            stopGame()
+        }
+    }          
+function stopGame(){ 
+   
+}
+
+    return { arr, testScore};
   }
-  
-  const board = createUser();
+  let movesMade = 0  
+  let winner = ""
 
+  const board = createUser();
+  
 
   allGrid.forEach((div)=>{
     div.addEventListener('click',(e)=>{
+        if(e.target.style.background === ""){
+        if(movesMade % 2 == 0){
         e.target.style.background = "red"
         board.arr[div.id] = "x"
         console.log(board.arr)
+        movesMade+=1
+        winner = "red"
+        }
+        else{
+            e.target.style.background = "blue"
+            board.arr[div.id] = "o"
+            console.log(board.arr)
+            movesMade+=1
+            winner = "blue"
+        }}
+        board.testScore()
     })
 })
 
-
-//   if(board.arr[0][0]===board.arr[0][1] &  board.arr[0][1]===board.arr[0][2] & board.arr[0][2] === "o"){
-//     console.log(`game over player{} win`)
-//     }else{console.log("help")}
 
 console.log(board.arr)
